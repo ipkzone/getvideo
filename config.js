8981659199871cfa163777992617fae093e3d2e2
Ferry Kirdan Agustin <33730041+ipkzone@users.noreply.github.com>
2020-01-30 09:20:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,13 +13,13 @@ var config = {
   errorTitle : "Page not Found (404) - GetVideo",
 
   //Application URL
-  url : "http://example.com",
+  url : "https://videozone.herokuapp.com/",
 
   //Application Description
   description : "Free online download YouTube videos, audio quickly in 1080p, 720p, MP4, 3GP, and more.",
 
   //Footer text
-  footerText : "getVideo &copy; 2018"
+  footerText : "getVideo &copy; 2020 Iddant ID"
 };
 
 module.exports = config;
